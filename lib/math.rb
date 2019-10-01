b5c6d1b68efab4078ca16f464f6837d3596e7b23
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-01 19:50:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,41 @@
-def addition(num1, num2)
-
+def addition (num1,num2)
+  num1 = 4
+  num2 = 2
+  addition = num1 + num2 
+  puts addition
 end
-
-def subtraction(num1, num2)
-
-end
-
+  
+  
+  def substraction(num1,num2)
+    num1 = 6
+    num2 = 3
+    substraction = num1 - num2
+    puts substraction
+  end 
+    
 def division(num1, num2)
-
+  num1 = 6
+  num2 = 2
+division= num1 / num2
+puts division
 end
 
-def multiplication(num1, num2)
-
+def multiplication (num1, num2)
+  num1 = 6
+  num2 = 2
+ multiplication = num1 * num2
+ puts multiplication
 end
 
 def modulo(num1, num2)
-
+   num1 = 4
+   num2 = 2
+   modulo = num1 % num2
+   puts modulo
 end
 
-def square_root(num)
-
+def square_root(num1)
+  num1 = 81
+  square_root= Math.sqrt (num1)
+  puts square_root
 end
